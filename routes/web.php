38deb70f9ae43aa68140bdddb02a2036da709270
LeBrontoJames <keenanabigailidn@gmail.com>
2025-02-15 17:02:54 +0700

    Final Resolve

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,6 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/users', [UserDashController::class, 'index'])->name('users.index');
 });
 
-
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
